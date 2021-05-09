159d63bd773801aacfc3748d3b5e94af56f3a930
[Shehan Mallawaarachchi] <[shehan96mts@gmail.com]>
2021-05-09 21:24:45 +0530

    Home page added

---
+++ app/src/main/java/com/example/traveller/dataBases/Trip.java
@@ -0,0 +1,51 @@
+package com.example.traveller.dataBases;
+
+public class Trip {
+
+    //helper classs
+    private String tname;
+    private String location;
+    private String discription;
+    public String imageURL;
+    public Trip() { }
+//constructor
+    public Trip(String tname, String location, String discription, String imageURL) {
+        this.tname = tname;
+        this.location = location;
+        this.discription = discription;
+        this.imageURL = imageURL;
+    }
+//getters and setters
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    public String getTname() {
+        return tname;
+    }
+
+    public void setTname(String tname) {
+        this.tname = tname;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDiscription() {
+        return discription;
+    }
+
+    public void setDiscription(String discription) {
+        this.discription = discription;
+    }
+}
---
+++ app/src/main/java/com/example/traveller/user/UserDashboard.java
@@ -0,0 +1,16 @@
+package com.example.traveller.user;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.traveller.R;
+
+public class UserDashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate ( savedInstanceState );
+        setContentView ( R.layout.activity_user_dashboard );
+    }
+}
